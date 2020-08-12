05acd3cc28ff409cba97c95232c5a047d26ded38
fab-cloud9 <69571305+fab-cloud9@users.noreply.github.com>
2020-08-12 15:46:40 +0200

    Ändrade kod helt till input/output-exempel

--- HelloMax.py
+++ HelloMax.py
@@ -1,15 +1,10 @@
-# This is a sample Python script.
+# Test code:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+name = input("What is your name: ")
+age = input("What is your age: ")
+age =int(age)
+die = 100 - age
+dead_year = 2020+die
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Maximiliano')
-
+print("Your name is " + name)
+print(" and you will live until " + str(dead_year))
