8748d187ba1f5f00da3323d8e5decc0f3eef13b7
fab-cloud9 <69571305+fab-cloud9@users.noreply.github.com>
2020-08-13 17:36:46 +0200

    Startded to only try out the Fibonacci series, but not done yet...

--- Master_File_Fibonacci.py
+++ Master_File_Fibonacci.py
@@ -1,11 +1,22 @@
 # This is the real Master file
 # Here there are changes made
 
+F_Num = 1  # value of Fibonacci number
+F_old_1 = 0  # latest old value of stored
+F_old_2 = 0  # oldest value of stored
+N = 3  # order of Fibonacci number
 i = 0
-x = 0
 
-while i < 5:
-    x = x + (i + 1)
+while i < N:
 
+    if N == 1:
+        print(F_Num)
+        F_Num = F_old_1
+    else:
+        F_old_2 = F_old_1
+        F_old_1 = F_Num
+        F_Num = F_old_1 + F_old_2
     i += 1
-print(x)
+print(F_Num)
+if N <= 0:
+    print("Can only apply positive integers")
