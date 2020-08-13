bf27364ed2010206019acc2cace1e23b8a409bbb
fab-cloud9 <69571305+fab-cloud9@users.noreply.github.com>
2020-08-13 14:45:47 +0200

    Created Test Code #1: To sort a random list in ascending order of all real numbers.

--- HelloMax.py
+++ HelloMax.py
@@ -1,11 +1,39 @@
-# Test code:
+# ----------- Code 1: Sort a list of numbers -------
+# 1 Get the list of numbers
+# 2. Sort them in ascending order
+# 3. display old and new sorting
 
-number = int(input("Input a number: "))
+start_list = [55, 3, 16, 77, 5]
+sort_list = []
+element = 0
+number = 0
 
-diff = number % 2 # if odd = 1, if even = 0 (no remainder)
-if (diff == 1):
-    print("The number " + str(number) + " is odd")
-else:
-    print("The number " + str(number) + " is even")
+# Fill up the list of numbers
+start_list = []
+n = int(input("Enter number of elements : "))
 
+# Iterating till the range
+for i in range(0, n):
+    x = int(input())
+    start_list.append(x)  # add element x
 
+print(start_list)
+lowest = start_list[0]
+
+# For the whole list until it is empty
+while len(start_list) > 0:
+    number = start_list[element]
+# Find the lowest number in list and place first
+    if number < lowest:
+        lowest = start_list[element]
+    element += 1
+# Determine if whole list have been walked through, remove/add lowest number to sorted list
+    if element == len(start_list):
+        sort_list.append(lowest)
+        start_list.remove(lowest)
+# As long as there is a list to pick from
+        if len(start_list) > 0:
+            lowest = start_list[0]
+        element = 0
+
+print(sort_list)
