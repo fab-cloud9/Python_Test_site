0ca6f72f2f24708f34f35f25eeaa35ae696b3c54
fab-cloud9 <69571305+fab-cloud9@users.noreply.github.com>
2020-08-18 10:32:12 +0200

    I added a small calculation to test the commit of this branch  with '#' will be ignored, and an empty message aborts the commit.
     On branch fab-cloud9-patch-1
     Changes to be committed:
    	modified:   Master_File_Fibonacci.py
    
     Untracked files:
    	.idea/

--- Master_File_Fibonacci.py
+++ Master_File_Fibonacci.py
@@ -1,22 +1,83 @@
 # This is the real Master file
-# Here there are changes made
 
-F_Num = 1  # value of Fibonacci number
-F_old_1 = 0  # latest old value of stored
-F_old_2 = 0  # oldest value of stored
-N = 3  # order of Fibonacci number
-i = 0
 
-while i < N:
+# ------------------ WHILE LOOP (ITERATIVE) Version -----------------------------------------
+# Create a While Loop
+# Iterate through the N
+# Fibonacci Numbers are: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,.....]
+# Fib_(N) = Fib_(N-1) + Fib_(N-2)
+# Print the response to the Nth number
 
-    if N == 1:
-        print(F_Num)
-        F_Num = F_old_1
+# F_New = 1  # new value of Fibonacci number
+# F_old_1 = 1  # latest old value of stored
+# F_old_2 = 0  # oldest value of stored
+# N = 5  # order of Fibonacci number
+# i = 0  # iterations
+#
+# while i < (N-2):
+#     i += 1
+#     if N == 1: # print the result of the 1st Fibonacci Number
+#         F_New = F_old_1
+#     else:
+#         F_old_2 = F_old_1
+#         F_old_1 = F_New
+#         F_New = F_old_1 + F_old_2 # Calculate the upcoming value for next iteration
+#
+#
+# if N <= 0:
+#     print("Can only apply positive integers")
+# elif N == 1: # The Nth number is the latest old value
+#     F_Num = F_old_2
+#     print(F_Num)
+# else:
+#     F_Num = F_old_1
+#     print(F_Num)
+
+# -------------- FOR LOOP (ITERATIVE) Version ---------------------------------------------
+# Create a function with input N
+# Iterate through the N
+# Fibonacci Numbers are: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,.....]
+# Fib_(N) = Fib_(N-1) + Fib_(N-2)
+# Print the response to the Nth number
+
+
+# def getfib(n):
+#
+#     fib_old_2 = 0  # 1st initial oldest value of stored Fibonacci Number
+#     fib_old_1 = 1  # 2nd initial latest old value of stored Fibonacci Number
+#     fib_for = 1    # 3rd initial fib number 2
+#
+#     for number in list(range(n-3)):  # 3 initial variables --> if N <= 3 use initial variables without iteration
+#         fib_old_2 = fib_old_1
+#         fib_old_1 = fib_for
+#         fib_for = fib_old_1 + fib_old_2
+#     if n == 1:
+#         return fib_old_2
+#     elif n == 2:
+#         return fib_old_1
+#     else:
+#         return fib_for
+#
+#
+# print(getfib(N))
+
+
+# ---------------------------- RECURSIVE Version ------------------------------------------------------------
+
+#  Create a function that has correct conditions
+N = 4
+def getFibboNumb(nn, fibbo_rec):
+    fibbo_old_2 = 0  # 1st initial oldest value of stored Fibonacci Number
+    fibbo_old_1 = 1  # 2nd initial latest old value of stored Fibonacci Number
+
+
+    if nn == 1:
+        return fibbo_old_2
+    elif nn == 2:
+        return fibbo_old_1
     else:
-        F_old_2 = F_old_1
-        F_old_1 = F_Num
-        F_Num = F_old_1 + F_old_2
-    i += 1
-print(F_Num)
-if N <= 0:
-    print("Can only apply positive integers")
+        return getFibboNumb(nn-1, fibbo_rec + fibbo_old_1 + fibbo_old_2)
+        fibbo_old_2 = fibbo_old_1
+        fibbo_old_1 = fibbo_rec
+
+print(getFibboNumb(N,0))
