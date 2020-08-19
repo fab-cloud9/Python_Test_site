8a6fb46da47d27b6c01f71818e80d4ea8631be12
fab-cloud9 <69571305+fab-cloud9@users.noreply.github.com>
2020-08-19 10:45:06 +0200

    Understanding of code and small fixes
    Here I corrected the order of choice. The Fibonacci number starts with 0
    in Python, but when you count the order it corrresponds to 1. Plus that
    the recursion ris understood now.

--- Master_File_Fibonacci.py
+++ Master_File_Fibonacci.py
@@ -1,62 +1,63 @@
 # This is the real Master file
 
-N = int(input("Which order of Fibonacci number are you interested in: ")) # order of Fibonacci number
+N = int(input("Which order of Fibonacci number are you interested in: ")) - 1  # order Fibonacci starts with 0th order
 answer = input("In what way do you want to find your Fibonacci Number, While Loop = w, For Loop = f or Recursice = r: ")
 
-if answer == "w":
-# ------------------ WHILE LOOP (ITERATIVE) Version -----------------------------------------
+if N <= 0:
+    print("ERROR: You must enter a positive integer")
+elif answer == "w":
+    # ------------------ WHILE LOOP (ITERATIVE) Version -----------------------------------------
     # Create a While Loop
     # Iterate through the N
     # Fibonacci Numbers are: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,.....]
     # Fib_(N) = Fib_(N-1) + Fib_(N-2)
     # Print the response to the Nth number
 
-    F_New = 1  # new value of Fibonacci number
     F_old_1 = 1  # latest old value of stored
     F_old_2 = 0  # oldest value of stored
     i = 0  # iterations
 
-    while i < (N-2):
-        i += 1
-        if N == 1: # print the result of the 1st Fibonacci Number
-            F_New = F_old_1
+    while i < (N - 1):
+
+        if N == 1:  # print the result of the 1st Fibonacci Number
+            F_Num = F_old_1
         else:
+            F_Num = F_old_1 + F_old_2  # Calculate the upcoming value for next iteration
             F_old_2 = F_old_1
-            F_old_1 = F_New
-            F_New = F_old_1 + F_old_2 # Calculate the upcoming value for next iteration
-
-
-    if N <= 0:
-        print("Can only apply positive integers")
-    elif N == 1: # The Nth number is the latest old value
+            F_old_1 = F_Num
+        i += 1
+    if N == 0:  # The first Fibonacci Number is already defined
         F_Num = F_old_2
         print(F_Num)
-    else:
+
+    elif N == 1:  # The second Fibonacci Number is already defined
         F_Num = F_old_1
+        print(F_Num)
+    else:
+        # F_Num = F_New
         print(F_Num)
 
 elif answer == "f":
-# -------------- FOR LOOP (ITERATIVE) Version ---------------------------------------------
+    # -------------- FOR LOOP (ITERATIVE) Version ---------------------------------------------
     # Create a function with input N
     # Iterate through the N
     # Fibonacci Numbers are: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,.....]
     # Fib_(N) = Fib_(N-1) + Fib_(N-2)
     # Print the response to the Nth number
 
-
     def getfib(n):
 
         fib_old_2 = 0  # 1st initial oldest value of stored Fibonacci Number
         fib_old_1 = 1  # 2nd initial latest old value of stored Fibonacci Number
-        fib_for = 1    # 3rd initial fib number 2
+        fib_for = 1  # 3rd initial fib number 2
 
-        for number in list(range(n-3)):  # 3 initial variables --> if N <= 3 use initial variables without iteration
+        for number in list(range(n - 2)):  # 3 initial variables --> if N <= 2 use initial variables without iteration
             fib_old_2 = fib_old_1
             fib_old_1 = fib_for
             fib_for = fib_old_1 + fib_old_2
-        if n == 1:
+        if n == 0:
             return fib_old_2
-        elif n == 2:
+        elif n == 1:
             return fib_old_1
         else:
             return fib_for
@@ -65,19 +66,24 @@ elif answer == "f":
     print(getfib(N))
 
 else:
-#---------------------------- RECURSIVE Version ------------------------------------------------------------
+    # ---------------------------- RECURSIVE Version ------------------------------------------------------------
     #  Create a function that has correct conditions
-    #  The recursive function will decrease the input number and re-call itself until it is zero
+    #  The recursive function will go down to the bottom (nn = 0) of the fibo_rec-tree
+    #  and find initial values fibo:old_2 & fibo_old_1
+    #  Then work its' way up the tree to the top
+
+    fibo_old_2 = 0  # 1st initial oldest value of stored Fibonacci Number
+    fibo_old_1 = 1  # 2nd initial latest old value of stored Fibonacci Number
 
 
     def fibo_rec(nn):
 
         if nn == 0:
-            return 0
+            return fibo_old_2
         elif nn == 1:
-            return 1
+            return fibo_old_1
         else:
-            return fibo_rec(nn-1) + fibo_rec(nn-2)
+            return fibo_rec(nn - 1) + fibo_rec(nn - 2)
 
 
-    print(fibo_rec(N-1))
+    print(fibo_rec(N))
